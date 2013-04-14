edb3b017970ed4f093fd049c0600a78c8045d241
kylebgorman <kylebgorman@gmail.com>
2013-04-14 14:13:20 -0700

    CMU dictionary's location changed...

--- get_dict.sh
+++ get_dict.sh
@@ -3,4 +3,4 @@
 ## Kyle Gorman <kgorman@ling.upenn.edu>
 
 ## use the following line to get a stress dictionary
-curl -s https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/cmudict/cmudict.0.7a | grep "^[A-Z]" | grep -v '^SIL  ' | sed -e 's/([0-9][0-9]*)//' | sed -e 's/\ \ */\ /g' | sed -e "s/^\'/\\\'/" | sed -e "s/ARABIA AH R EY1 B IY0 AH0/ARABIA AH0 R EY1 B IY0 AH0/" > dictionary.txt
+curl -s http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict.0.7a | grep "^[A-Z]" | grep -v '^SIL  ' | sed -e 's/([0-9][0-9]*)//' | sed -e 's/\ \ */\ /g' | sed -e "s/^\'/\\\'/" | sed -e "s/ARABIA AH R EY1 B IY0 AH0/ARABIA AH0 R EY1 B IY0 AH0/" > dictionary.txt
