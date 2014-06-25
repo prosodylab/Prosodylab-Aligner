34535f30b62f0d74e9183da3fdd3dd0e6ae14b8b
Kyle Gorman <gormanky@ohsu.edu>
2014-06-25 16:53:26 -0700

    rounding in `textgrid.py`
    Erin Olson reports a problem with floating point rounding while reading
    an MLF file. It's not clear to me why this is happening, but reducing
    the precision to 1ms seems to have the desired effect.

--- textgrid.py
+++ textgrid.py
@@ -646,8 +646,8 @@ class TextGrid(object):
         source.readline() # header junk
         source.readline() # header junk
         source.readline() # header junk
-        self.minTime = round(float(source.readline().split()[2]), 5)
-        self.maxTime = round(float(source.readline().split()[2]), 5)
+        self.minTime = round(float(source.readline().split()[2]), 4)
+        self.maxTime = round(float(source.readline().split()[2]), 4)
         source.readline() # more header junk
         m = int(source.readline().rstrip().split()[2]) # will be self.n
         source.readline()
@@ -655,27 +655,27 @@ class TextGrid(object):
             source.readline()
             if source.readline().rstrip().split()[2] == '"IntervalTier"': 
                 inam = source.readline().rstrip().split(' = ')[1].strip('"')
-                imin = round(float(source.readline().rstrip().split()[2]), 5)
-                imax = round(float(source.readline().rstrip().split()[2]), 5)
+                imin = round(float(source.readline().rstrip().split()[2]), 4)
+                imax = round(float(source.readline().rstrip().split()[2]), 4)
                 itie = IntervalTier(inam)
                 for j in xrange(int(source.readline().rstrip().split()[3])):
                     source.readline().rstrip().split() # header junk
-                    jmin = round(float(source.readline().rstrip().split()[2]), 5)
-                    jmax = round(float(source.readline().rstrip().split()[2]), 5)
+                    jmin = round(float(source.readline().rstrip().split()[2]), 4)
+                    jmax = round(float(source.readline().rstrip().split()[2]), 4)
                     jmrk = self._getMark(source)
                     if jmin < jmax: # non-null
                         itie.addInterval(Interval(jmin, jmax, jmrk))
                 self.append(itie)
             else: # pointTier
                 inam = source.readline().rstrip().split(' = ')[1].strip('"')
-                imin = round(float(source.readline().rstrip().split()[2]), 5)
-                imax = round(float(source.readline().rstrip().split()[2]), 5)
+                imin = round(float(source.readline().rstrip().split()[2]), 4)
+                imax = round(float(source.readline().rstrip().split()[2]), 4)
                 itie = PointTier(inam)
                 n = int(source.readline().rstrip().split()[3])
                 for j in xrange(n):
                     source.readline().rstrip() # header junk
                     jtim = round(float(source.readline().rstrip().split()[2]),
-                                                                           5)
+                                                                           4)
                     jmrk = source.readline().rstrip().split()[2][1:-1]
                     itie.addPoint(Point(jtim, jmrk))
                 self.append(itie)
@@ -795,8 +795,8 @@ class MLF(object):
                 while 1: # loop over the lines in each grid
                     line = source.readline().rstrip().split()
                     if len(line) == 4: # word on this baby
-                        pmin = round(float(line[0]) / samplerate, 5)
-                        pmax = round(float(line[1]) / samplerate, 5)
+                        pmin = round(float(line[0]) / samplerate, 4)
+                        pmax = round(float(line[1]) / samplerate, 4)
                         if pmin == pmax:
                             raise ValueError('null duration interval')
                         phon.add(pmin, pmax, line[2])
@@ -806,8 +806,8 @@ class MLF(object):
                         wsrt = pmin
                         wend = pmax
                     elif len(line) == 3: # just phone
-                        pmin = round(float(line[0]) / samplerate, 5)
-                        pmax = round(float(line[1]) / samplerate, 5)
+                        pmin = round(float(line[0]) / samplerate, 4)
+                        pmax = round(float(line[1]) / samplerate, 4)
                         if line[2] == 'sp' and pmin != pmax:
                             if wmrk:
                                 word.add(wsrt, wend, wmrk)
